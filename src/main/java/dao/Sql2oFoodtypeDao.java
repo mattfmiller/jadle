d5c7630a1e369b38f6814ba009bbec0dc2a22add
Matt Dowdney and Matt Miller <matt+matt@>
2018-06-11 08:21:19 -0700

    Create initial project structure from lesson.

--- src/main/java/dao/Sql2oFoodtypeDao.java
+++ src/main/java/dao/Sql2oFoodtypeDao.java
@@ -1,4 +1,27 @@
 package dao;
 
-public class Sql2oFoodtypeDao {
+import models.Foodtype;
+
+import java.util.List;
+
+public class Sql2oFoodtypeDao implements FoodtypeDao {
+    @Override
+    public void add(Foodtype foodtype) {
+
+    }
+
+    @Override
+    public List<Foodtype> getAll() {
+        return null;
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void clearAll() {
+
+    }
 }
--- src/main/java/dao/Sql2oRestaurantDao.java
+++ src/main/java/dao/Sql2oRestaurantDao.java
@@ -1,4 +1,37 @@
 package dao;
 
-public class Sql2oRestaurantDao {
+import models.Restaurant;
+
+import java.util.List;
+
+public class Sql2oRestaurantDao implements RestaurantDao {
+    @Override
+    public void add(Restaurant restaurant) {
+
+    }
+
+    @Override
+    public List<Restaurant> getAll() {
+        return null;
+    }
+
+    @Override
+    public Restaurant findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void update(int id, String name, String address, String zipcode, String phone, String website, String email) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void clearAll() {
+
+    }
 }
