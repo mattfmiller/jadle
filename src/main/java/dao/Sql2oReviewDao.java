daf99fb0b51f173ff357d1088bffbe6931d904b8
Matt Miller <matt.f.miller@live.com>
2018-06-13 13:49:11 -0700

    Update compareTo method to only apply to lists longer than one review.

--- src/main/java/dao/Sql2oReviewDao.java
+++ src/main/java/dao/Sql2oReviewDao.java
@@ -66,14 +66,16 @@ public class Sql2oReviewDao implements ReviewDao {
     @Override
     public List<Review> getAllReviewsByRestaurantIdSortedNewestToOldest(int restaurantId) {
         List<Review> reviews = getAllReviewsByRestaurantId(restaurantId);
-        for (Integer i=0; i < reviews.size(); i++) {
-            for (Integer index = 0; index < reviews.size() -1; index ++) {
-                Review review1 = reviews.get(index);
-                Review review2 = reviews.get(index + 1);
-                int compareResult = review1.compareTo(reviews.get(index + 1));
-                if (compareResult == -1) {
-                    reviews.set(index, review2);
-                    reviews.set(index + 1, review1);
+        if ( reviews.size() > 1) {
+            for (Integer i=0; i < reviews.size(); i++) {
+                for (Integer index = 0; index < reviews.size() -1; index ++) {
+                    Review review1 = reviews.get(index);
+                    Review review2 = reviews.get(index + 1);
+                    int compareResult = review1.compareTo(reviews.get(index + 1));
+                    if (compareResult == -1) {
+                        reviews.set(index, review2);
+                        reviews.set(index + 1, review1);
+                    }
                 }
             }
         }
